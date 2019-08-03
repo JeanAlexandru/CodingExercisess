c56deb9fa69625aa4839617c25c3214f1ccb25e7
Andrei.Alexandru88 <andreimialexandru@gmail.com>
2019-08-03 12:31:37 +0300

    //ce am lucrat astazi J28

---
+++ src/OOP_PersonalTrainer/Suplements.java
@@ -0,0 +1,31 @@
+package OOP_PersonalTrainer;
+
+public class Suplements {
+    private double vitamin=2;
+    private double multiplier=10;
+
+    public Suplements(){
+
+    }
+
+    public Suplements(double vitamin, double multiplier){
+        this.vitamin=vitamin;
+        this.multiplier=multiplier;
+    }
+
+    public double getVitamin() {
+        return vitamin;
+    }
+
+    public void setVitamin(double vitamin) {
+        this.vitamin = vitamin;
+    }
+
+    public double getMultiplier() {
+        return multiplier;
+    }
+
+    public void setMultiplier(double multiplier) {
+        this.multiplier = multiplier;
+    }
+}
